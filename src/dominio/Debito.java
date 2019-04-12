bf0354afa55ed7ad642016657b6e05fd3194dd92
dw1b_alum12 <dw1b_alum12@DW1B-02.inform.local>
2019-04-12 13:56:05 +0200

    Clases Copiadas Parte Ander

---
+++ src/dominio/Debito.java
@@ -0,0 +1,30 @@
+package dominio;
+
+public class Debito {
+
+	public Debito(String numero, String titular, Date fechaCaducidad) {
+		super(numero, titular, fechaCaducidad);
+	}
+	
+	
+	public void retirar(double x) throws Exception{
+		this.mCuentaAsocida.retirar("Retirada en cajero automática", x);
+	}
+	
+	
+	public void ingresar(double x) throws Exception{
+		this.mCuentaAsociada.retirar("Ingreso en cajero automatico", x);
+	}
+	
+	
+	public void pagoEnEstablecimiento(String datos, double x) throws Exception{
+		this.mCuentaAsociada.retirar("Compra en: "+datos,x);
+	}
+	
+	
+	public double getSaldo() {
+		return mCuentaAsociada.getSaldo();
+	}
+	
+	
+}
